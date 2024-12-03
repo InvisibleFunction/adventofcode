4530f190bb6ac6fef2e5a92418b10efb4b4c0aeb
Dan Weintraub <93551197+DanielWeintraub@users.noreply.github.com>
2024-12-03 09:45:05 -0500

    2024 Day 3

--- 2024/03b.py
+++ 2024/03b.py
@@ -8,13 +8,19 @@ inFile = sys.argv[1]
 
 with open(inFile,'r') as f:
     lines = f.read().splitlines()
+myinput = ""
+for line in lines:
+    myinput = myinput + " " + line
 
 mysum = 0
-for line in lines:
-    pattern = r"mul\((-?\d+),(-?\d+)\)"
-    matches = re.findall(pattern, line)
-    for match in matches:
-        mysum += int(match[0]) * int(match[1])
+pattern1 = r"don\'t\(\).*?do\(\)"
+input2 = re.sub(pattern1, "", myinput)
+pattern2 = r"don\'t\(\).*$"
+input3 = re.sub(pattern2, "", input2)
+pattern3 = r"mul\((-?\d+),(-?\d+)\)"
+matches = re.findall(pattern3, input3)
+for match in matches:
+    mysum += int(match[0]) * int(match[1])
 
 ic(mysum)
 
